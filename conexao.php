2225595f08af6c1b62033c42d0d08a4764a5b7ca
Isabelle Pacheco <isapachecosenai@gmail.com>
2025-01-28 11:35:55 -0300

    conexao

---
+++ conexao.php
@@ -0,0 +1,27 @@
+<?php
+
+// Alterar de acordo com o numero da porta
+// verificar conexão com XAMP
+$servername = "localhost:3309";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+try {
+    // conexao com dados de banco
+    // conferir se as informações estão corretas
+    $conn = new mysqli ($servername, $username, $password, $dbname);
+
+    // verifica se houve algum erro na conexao
+    if ($conn->connect_error){
+        throw new Exception("Falha na conexão: ". $conn->connect_error);
+    }
+
+    echo "Conexão realizada com sucesso!";
+
+} catch (Exception $e){
+    // exibe uma mensagem de erro amigável
+    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
+}
+
+?>
